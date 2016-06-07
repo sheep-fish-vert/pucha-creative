7e979b9ced6ae1c1d9fc5b939f7a93b4c31f2d9b
radiks <radion91@gmail.com>
2016-06-07 09:34:47 +0300

    fullpage

--- src/js/basic_scripts.js
+++ src/js/basic_scripts.js
@@ -156,16 +156,78 @@ function headeButer(menuMobile,toggleMenu){
 
 /* /expresion for numbers with spaces */
 
+function fullPage(){
+    $('.fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['main', 'services','portfolio','our-team','contacts'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        //css3: true,
+        scrollingSpeed: 700,
+        //autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        //loopBottom: false,
+        //loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        //normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        //sectionsColor : ['#ccc', '#fff'],
+        //paddingTop: '3em',
+        //paddingBottom: '10px',
+        fixedElements: '.header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+
+        //Custom selectors
+        sectionSelector: 'section',
+        //slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+
+}
+
+
+
 /* DOCUMENT READY  */
 $(document).ready(function() {
     //oneHeightItems();
-    $('.footer_placeholder').height($('.footer').outerHeight());
-
+    fullPage();
     //goTo();
     //animationBlock($('.setion-animate'));
 });
 
 $(window).resize(function() {
 
-    $('.footer_placeholder').height($('.footer').outerHeight());
 });
